ad46c3098cc07e3e727e08e520007764a16fdd53
Rick Cheng <rcsw.devel@gmail.com>
2019-10-15 15:44:51 -0700

    Update Tank prefab to Plane with fixed rotation

---
+++ Assets/Scripts/Utilities/FixedRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tanks.Utilities { 
+    public class FixedRotation : MonoBehaviour
+    {
+        [SerializeField]
+        Quaternion rotation;
+        void Awake()
+        {
+            rotation = transform.rotation;
+        }
+        void LateUpdate()
+        {
+            transform.rotation = rotation;
+        }
+    }
+}
